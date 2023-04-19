eb0e8c38a9d071410a104746227368b269f45ef6
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-04-19 14:56:38 +0200

    cercaEdificioPerId

--- spring_jpa_progettoW5/src/main/java/com/jpa_project/controller/EdificioController.java
+++ spring_jpa_progettoW5/src/main/java/com/jpa_project/controller/EdificioController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,8 @@ public class EdificioController {
 		return new ResponseEntity<List<Edificio>>(edificioService.cercaTuttiEdifici(), HttpStatus.OK);
 	}
 	
-	
+	@GetMapping("/{id}")
+	public ResponseEntity<Edificio> getEdifici(@PathVariable Long id){
+		return new ResponseEntity<Edificio>(edificioService.cercaEdificioPerId(id), HttpStatus.OK);
+	}
 }
